5e31fa18fd0a8c610958a1c84a0cb95c9f62ea7d
4lkaid <4lkaid@gmail.com>
2024-01-04 20:46:25 +0800

    feat(middleware): introduce X_REQUEST_ID constant and customize request tracing

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,3 +1,5 @@
 pub mod cors;
 pub mod request_id;
 pub mod trace;
+
+pub const X_REQUEST_ID: &str = "x-request-id";
--- src/middleware/trace.rs
+++ src/middleware/trace.rs
@@ -1,8 +1,36 @@
+// use tower_http::{
+//     classify::{ServerErrorsAsFailures, SharedClassifier},
+//     trace::{DefaultMakeSpan, TraceLayer},
+// };
+
+// pub fn trace() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>> {
+//     TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::new().include_headers(true))
+// }
+
+use super::X_REQUEST_ID;
+use axum::{extract::Request, http::HeaderName};
 use tower_http::{
     classify::{ServerErrorsAsFailures, SharedClassifier},
-    trace::{DefaultMakeSpan, TraceLayer},
+    trace::{MakeSpan, TraceLayer},
 };
+use tracing::Span;
 
-pub fn trace() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>> {
-    TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::new().include_headers(true))
+#[derive(Clone)]
+pub struct CustomMakeSpan;
+
+impl<B> MakeSpan<B> for CustomMakeSpan {
+    fn make_span(&mut self, req: &Request<B>) -> Span {
+        let method = req.method();
+        let uri = req.uri();
+        let x_request_id = req
+            .headers()
+            .get(HeaderName::from_static(X_REQUEST_ID))
+            .and_then(|value| value.to_str().ok())
+            .unwrap_or("N/A");
+        tracing::debug_span!("request", %method, %uri, {X_REQUEST_ID} = %x_request_id)
+    }
+}
+
+pub fn trace() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>, CustomMakeSpan> {
+    TraceLayer::new_for_http().make_span_with(CustomMakeSpan)
 }
