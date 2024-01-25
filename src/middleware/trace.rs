08a8aa2b3b074076c14a1f73707daaa4901a4718
4lkaid <4lkaid@gmail.com>
2024-01-25 17:00:54 +0800

    refactor: improve trace middleware for enhanced logging

--- src/middleware/trace.rs
+++ src/middleware/trace.rs
@@ -5,47 +5,80 @@ use tower_http::{
     classify::{ServerErrorsAsFailures, SharedClassifier},
     trace::{MakeSpan, TraceLayer},
 };
-use tracing::Span;
+use tracing::{Level, Span};
 
-#[derive(Clone)]
-pub struct CustomMakeSpan;
+const DEFAULT_MESSAGE_LEVEL: Level = Level::DEBUG;
+
+#[derive(Debug, Clone)]
+pub struct CustomMakeSpan {
+    level: Level,
+    include_headers: bool,
+}
+
+impl CustomMakeSpan {
+    pub fn new() -> Self {
+        Self {
+            level: DEFAULT_MESSAGE_LEVEL,
+            include_headers: false,
+        }
+    }
+
+    pub fn level(mut self, level: Level) -> Self {
+        self.level = level;
+        self
+    }
+
+    pub fn include_headers(mut self, include_headers: bool) -> Self {
+        self.include_headers = include_headers;
+        self
+    }
+}
 
 impl<B> MakeSpan<B> for CustomMakeSpan {
     fn make_span(&mut self, req: &Request<B>) -> Span {
-        let method = req.method();
-        let uri = req.uri();
-        let x_request_id = req
-            .headers()
-            .get(HeaderName::from_static(X_REQUEST_ID))
-            .and_then(|value| value.to_str().ok())
-            .unwrap_or("N/A");
         let direct_connect_ip = req
             .extensions()
             .get::<axum::extract::connect_info::ConnectInfo<SocketAddr>>()
             .map(|addr| addr.ip().to_string())
             .unwrap_or("N/A".to_string());
-        let x_forwarded_for = req
-            .headers()
-            .get(HeaderName::from_static(X_FORWARDED_FOR))
-            .and_then(|value| value.to_str().ok())
-            .unwrap_or("N/A");
-        let x_real_ip = req
-            .headers()
-            .get(HeaderName::from_static(X_REAL_IP))
-            .and_then(|value| value.to_str().ok())
-            .unwrap_or("N/A");
-        tracing::debug_span!(
-            "request",
-            %method,
-            %uri,
-            {X_REQUEST_ID} = %x_request_id,
-            {DIRECT_CONNECT_IP} = %direct_connect_ip,
-            {X_FORWARDED_FOR} = %x_forwarded_for,
-            {X_REAL_IP} = %x_real_ip
-        )
+        macro_rules! make_span {
+            ($level:expr) => {
+                if self.include_headers {
+                    tracing::span!(
+                        $level,
+                        "request",
+                        {DIRECT_CONNECT_IP} = %direct_connect_ip,
+                        method = %req.method(),
+                        uri = %req.uri(),
+                        version = ?req.version(),
+                        headers = ?req.headers(),
+                    )
+                } else {
+                    tracing::span!(
+                        $level,
+                        "request",
+                        {DIRECT_CONNECT_IP} = %direct_connect_ip,
+                        {X_FORWARDED_FOR} = %req.headers().get(HeaderName::from_static(X_FORWARDED_FOR)).and_then(|value| value.to_str().ok()).unwrap_or("N/A"),
+                        {X_REAL_IP} = %req.headers().get(HeaderName::from_static(X_REAL_IP)).and_then(|value| value.to_str().ok()).unwrap_or("N/A"),
+                        {X_REQUEST_ID} = %req.headers().get(HeaderName::from_static(X_REQUEST_ID)).and_then(|value| value.to_str().ok()).unwrap_or("N/A"),
+                        method = %req.method(),
+                        uri = %req.uri(),
+                        version = ?req.version(),
+                    )
+                }
+            }
+        }
+
+        match self.level {
+            Level::ERROR => make_span!(Level::ERROR),
+            Level::WARN => make_span!(Level::WARN),
+            Level::INFO => make_span!(Level::INFO),
+            Level::DEBUG => make_span!(Level::DEBUG),
+            Level::TRACE => make_span!(Level::TRACE),
+        }
     }
 }
 
 pub fn trace() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>, CustomMakeSpan> {
-    TraceLayer::new_for_http().make_span_with(CustomMakeSpan)
+    TraceLayer::new_for_http().make_span_with(CustomMakeSpan::new())
 }
