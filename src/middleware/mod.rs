5f3bca19bff00b762754e06c5bcf97afafd49697
4lkaid <4lkaid@gmail.com>
2024-03-05 15:18:23 +0800

    feat(middleware): add request_response_logger middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,5 +1,6 @@
 pub mod cors;
 pub mod request_id;
+pub mod request_response_logger;
 pub mod trace;
 
 pub const DIRECT_CONNECT_IP: &str = "direct-connect-ip";
--- src/route/api.rs
+++ src/route/api.rs
@@ -1,8 +1,9 @@
 use crate::{
     handler,
-    middleware::{cors, request_id, trace},
+    middleware::{cors, request_id, request_response_logger, trace},
 };
 use axum::{
+    middleware,
     routing::{get, post},
     Router,
 };
@@ -12,6 +13,9 @@ pub fn init() -> Router {
     Router::new()
         .route("/", get(handler::demo::root))
         .route("/users", post(handler::demo::create_user))
+        .layer(middleware::from_fn(
+            request_response_logger::print_request_response,
+        ))
         // Its recommended to use tower::ServiceBuilder to apply multiple middleware at once, instead of calling layer (or route_layer) repeatedly.
         // ServiceBuilder works by composing all layers into one such that they run top to bottom.
         // Executing middleware top to bottom is generally easier to understand and follow mentally which is one of the reasons ServiceBuilder is recommended.
