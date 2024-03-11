3941644a8bbe68a460656fe1a0af971ffd589d61
4lkaid <4lkaid@gmail.com>
2024-03-11 10:55:29 +0800

    build: update redis 0.24 -> 0.25

--- src/config/redis.rs
+++ src/config/redis.rs
@@ -7,15 +7,15 @@ static REDIS: OnceLock<redis::Client> = OnceLock::new();
 pub async fn init(config: &Config) -> Result<()> {
     let url = config.get_string("redis.url")?;
     let client = redis::Client::open(url)?;
-    let _ = client.get_tokio_connection().await?;
+    let _ = client.get_multiplexed_tokio_connection().await?;
     let _ = REDIS.set(client);
     Ok(())
 }
 
-pub async fn conn() -> Result<redis::aio::Connection> {
+pub async fn conn() -> Result<redis::aio::MultiplexedConnection> {
     Ok(REDIS
         .get()
         .unwrap_or_else(|| panic!("redis client not initialized"))
-        .get_tokio_connection()
+        .get_multiplexed_tokio_connection()
         .await?)
 }
